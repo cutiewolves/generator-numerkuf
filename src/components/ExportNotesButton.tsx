da465da9fd35048ad0669ab379495c8e1d7fd11b
[dyad] <ilovetinystars@gmail.com>
2025-10-07 18:03:13 +0200

    [dyad] Simplifying export menu and fixing highlight color - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/ExportNotesButton.tsx
+++ src/components/ExportNotesButton.tsx
@@ -4,10 +4,6 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu';
 import { FileDown, ChevronDown } from 'lucide-react';
@@ -25,6 +21,8 @@ const ExportNotesButton = ({ notes, disabled }: ExportNotesButtonProps) => {
     handler(notes);
   };
 
+  const itemClassName = "focus:bg-gray-600 focus:text-white";
+
   return (
     <div className="flex items-center">
        <DropdownMenu>
@@ -33,62 +31,27 @@ const ExportNotesButton = ({ notes, disabled }: ExportNotesButtonProps) => {
             <ChevronDown className="h-4 w-4" />
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align="end" className="bg-gray-800 border-gray-700 text-white">
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Dokumenty</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent className="bg-gray-800 border-gray-700 text-white">
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handlePdf)}>PDF</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleDocx)}>DOCX (Word)</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleHtml)}>HTML</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleMarkdown)}>Markdown</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleTxt)}>TXT</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Arkusze</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent className="bg-gray-800 border-gray-700 text-white">
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleXlsx)}>XLSX (Excel)</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleCsv)}>CSV</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleTsv)}>TSV</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Dane</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent className="bg-gray-800 border-gray-700 text-white">
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleJson)}>JSON</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleJsonL)}>JSONL</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleXml)}>XML</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleYaml)}>YAML</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleSql)}>SQL (Insert)</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
-          <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Kod</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent className="bg-gray-800 border-gray-700 text-white">
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handlePhp)}>PHP</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handlePython)}>Python</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleRuby)}>Ruby</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
+        <DropdownMenuContent align="end" className="bg-gray-800 border-gray-700 text-white max-h-96 overflow-y-auto">
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handlePdf)}>PDF</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleDocx)}>DOCX (Word)</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleXlsx)}>XLSX (Excel)</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleHtml)}>HTML</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleMarkdown)}>Markdown</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleTxt)}>TXT</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleCsv)}>CSV</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleTsv)}>TSV</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleJson)}>JSON</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleJsonL)}>JSONL</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleXml)}>XML</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleYaml)}>YAML</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleSql)}>SQL (Insert)</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handlePhp)}>PHP</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handlePython)}>Python</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleRuby)}>Ruby</DropdownMenuItem>
           <DropdownMenuSeparator className="bg-gray-700" />
-           <DropdownMenuSub>
-            <DropdownMenuSubTrigger>Kopiuj do schowka</DropdownMenuSubTrigger>
-            <DropdownMenuPortal>
-              <DropdownMenuSubContent className="bg-gray-800 border-gray-700 text-white">
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleCopyToClipboardTxt)}>jako TXT</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleCopyToClipboardMd)}>jako Markdown</DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleExport(exportUtils.handleCopyToClipboardJson)}>jako JSON</DropdownMenuItem>
-              </DropdownMenuSubContent>
-            </DropdownMenuPortal>
-          </DropdownMenuSub>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleCopyToClipboardTxt)}>Kopiuj jako TXT</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleCopyToClipboardMd)}>Kopiuj jako Markdown</DropdownMenuItem>
+          <DropdownMenuItem className={itemClassName} onClick={() => handleExport(exportUtils.handleCopyToClipboardJson)}>Kopiuj jako JSON</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
       <Button onClick={() => handleExport(exportUtils.handleTxt)} disabled={disabled} variant="outline" className="bg-gray-700 border-gray-600 hover:bg-gray-600 rounded-l-none">
